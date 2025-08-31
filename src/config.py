9ed1ad78ace7d0eb544b25a05b36e9ceb1cdda93
oscarl77 <oscarlaurens18@gmail.com>
2025-09-01 00:02:55 +0200

    Populated config file

---
+++ src/config.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://www.cwjobs.co.uk/jobs/"
+URL_TAIL = "?page={page_number}&rsearch=1&searchOrigin=Homepage_top-search"
+ARTICLE_CLASS = "article.res-1c3sls7"
+JOB_TITLE = "Data Engineer"
+PAGES_TO_SCRAPE = 4
